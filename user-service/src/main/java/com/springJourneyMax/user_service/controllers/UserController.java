a1246264400e429bbd53031f13421c86614ac802
Rohan Uke <ukerohan7@gmail.com>
2025-06-13 01:07:19 +0530

    create a topic if not exist with name - user-random-topic

--- user-service/src/main/java/com/springJourneyMax/user_service/controllers/UserController.java
+++ user-service/src/main/java/com/springJourneyMax/user_service/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.springJourneyMax.user_service.controllers;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,10 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
+    @Value("${kafka.topic.user-random-topic}")
+    private String KAFKA_RANDOM_USER_TOPIC;
+
     private final KafkaTemplate<String,String> kafkaTemplate;
     @PostMapping("/{message}")
     public ResponseEntity<String> sendMsg(@PathVariable String message){
-        kafkaTemplate.send("user-random-topic",message);
+        kafkaTemplate.send(KAFKA_RANDOM_USER_TOPIC,message);
         return ResponseEntity.ok("Message Send");
     }
 
